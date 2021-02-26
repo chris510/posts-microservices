86352ac21aa56948974574166d3854fb47802479
Chris Trinh <christrinh5@gmail.com>
2021-02-25 22:43:58 -0800

    Add kubenetes, docker, and skaffolding files

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,10 +16,10 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event); // Post
-    axios.post('http://localhost:4001/events', event); // Comments
-    axios.post('http://localhost:4002/events', event); // Query
-    axios.post('http://localhost:4003/events', event); // Moderation
+    axios.post('http://posts-clusterip-srv:4000/events', event); // Post
+    axios.post('http://comments-clusterip-srv:4001/events', event); // Comments
+    axios.post('http://query-clusterip-srv:4002/events', event); // Query
+    axios.post('http://moderation-clusterip-srv:4003/events', event); // Moderation
 
     res.send({ status: 'Ok' });
 })
